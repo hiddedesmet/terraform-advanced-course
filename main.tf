7c27c08ab8478656d349aabc0935eb86656290a4
Hidde de Smet <desmet.hidde@hotmail.com>
2025-05-30 16:17:44 +0200

    fix: Add subscription_id parameter to keyvault module

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ module "webapp" {
 
 module "keyvault" {
   source = "./modules/keyvault"
-  
+
   subscription_id            = var.subscription_id
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
